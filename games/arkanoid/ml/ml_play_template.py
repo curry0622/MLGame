5cc05475b1afb4bde4ff961e8cb819ee45f54895
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-11 17:57:38 +0800

    feat: predict and move

--- games/arkanoid/ml/ml_play_template.py
+++ games/arkanoid/ml/ml_play_template.py
@@ -9,7 +9,9 @@ class MLPlay:
         """
         self.ball_served = False
         self.curr_x = 0
+        self.curr_y = 0
         self.last_x = 0
+        self.last_y = 0
 
     def update(self, scene_info):
         """
@@ -22,18 +24,37 @@ class MLPlay:
 
         if not self.ball_served:
             self.ball_served = True
-            command = "SERVE_TO_LEFT"
             self.last_x = scene_info["ball"][0]
+            self.last_y = scene_info["ball"][1]
+            command = "SERVE_TO_RIGHT"
         else:
             self.curr_x = scene_info["ball"][0]
-            if self.curr_x - self.last_x > 0:
+            self.curr_y = scene_info["ball"][1]
+            predict_x = self.predict()
+            if predict_x == -1:
+                command = "NONE"
+            elif predict_x > scene_info["platform"][0]:
                 command = "MOVE_RIGHT"
-            elif self.curr_x - self.last_x < 0:
+            elif predict_x < scene_info["platform"][0]:
                 command = "MOVE_LEFT"
             else:
                 command = "NONE"
-            self.last_x = scene_info["ball"][0]
+            self.last_x = self.curr_x
+            self.last_y = self.curr_y
+        print(command)
         return command
+
+    def predict(self):
+        # print(self.curr_y, self.last_y)
+        if self.curr_y - self.last_y > 0:
+            # dropping
+            delta_x = self.curr_x - self.last_x
+            delta_y = self.curr_y - self.last_y
+            slope = delta_y / delta_x
+            # print("down")
+            return (400 - self.curr_y + slope * self.curr_x) / slope
+        # print("up")
+        return -1
 
     def reset(self):
         """
