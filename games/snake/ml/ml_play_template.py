e11cc15dfebf9248727b0d5277db1fabd2d898a3
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-06-03 15:20:27 +0800

    feat: basic info

--- games/snake/ml/ml_play_template.py
+++ games/snake/ml/ml_play_template.py
@@ -16,20 +16,36 @@ class MLPlay:
         if scene_info["status"] == "GAME_OVER":
             return "RESET"
 
-        snake_head = scene_info["snake_head"]
-        food = scene_info["food"]
+        # set scene info
+        self.scene_info = scene_info
 
-        if snake_head[0] > food[0]:
-            return "LEFT"
-        elif snake_head[0] < food[0]:
-            return "RIGHT"
-        elif snake_head[1] > food[1]:
-            return "UP"
-        elif snake_head[1] < food[1]:
-            return "DOWN"
+        # set useful info
+        self.head = scene_info["snake_head"]
+        self.neck = scene_info["snake_body"][1]
+        self.food = scene_info["food"]
+        self.set_dir()
+
+        return self.dir
+        # if snake_head[0] > food[0]:
+        #     return "LEFT"
+        # elif snake_head[0] < food[0]:
+        #     return "RIGHT"
+        # elif snake_head[1] > food[1]:
+        #     return "UP"
+        # elif snake_head[1] < food[1]:
+        #     return "DOWN"
 
     def reset(self):
         """
         Reset the status if needed
         """
         pass
+
+    def set_dir(self):
+        if self.head[0] == self.neck[0] and self.head[1] < self.neck[1]:
+            self.dir = "UP"
+        elif self.head[0] == self.neck[0] and self.head[1] > self.neck[1]:
+            self.dir = "DOWN"
+        elif self.head[1] == self.neck[1] and self.head[0] < self.neck[0]:
+            self.dir = "LEFT"
+        self.dir = "RIGHT"
